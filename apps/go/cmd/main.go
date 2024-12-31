8be5e92e64ba72ffafb396ef7ab206d88dc5b44c
Arun Singh <arunsingh28aug.as@gmail.com>
2024-12-31 15:15:05 +0530

    feat: implement HTTP server with graceful shutdown and logging

--- apps/go/cmd/main.go
+++ apps/go/cmd/main.go
@@ -1,5 +1,56 @@
 package main
 
-func main() {
+import (
+	"context"
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
+	"github.com/arunsingh28/openaxis/internal/config"
+)
+
+func main() {
+	cfg := config.MustLoad()
+
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {})
+
+	server := http.Server{
+		Addr:    cfg.HTTPServer.Address,
+		Handler: router,
+	}
+
+	slog.Info("starting server", slog.String("address", cfg.HTTPServer.Address))
+
+	// channel to listen for server shutdown
+	done := make(chan os.Signal, 1)
+
+	signal.Notify(done, os.Interrupt, os.Kill, syscall.SIGINT, syscall.SIGTERM)
+
+	// routine to start the server
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			log.Fatalf("failed to start server: %f", err.Error())
+		}
+	}()
+
+	<-done
+
+	slog.Info("server is shutting down", slog.String("address", cfg.HTTPServer.Address))
+
+	context, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err := server.Shutdown(context)
+	if err != nil {
+		slog.Info("failed to shutdown server", slog.String("address", cfg.HTTPServer.Address), slog.String("error", err.Error()))
+	}
+
+	slog.Info("server is shutdown", slog.String("address", cfg.HTTPServer.Address))
 }
